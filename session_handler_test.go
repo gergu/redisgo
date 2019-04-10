3cab974ea47b7da01363454c6c5ea0e26fdb622b
Grzegorz Korzeniowski <korzeniowski.g@gmail.com>
2019-04-10 23:22:07 +0200

    Add tests for getValue and auth functions

--- session_handler_test.go
+++ session_handler_test.go
@@ -22,5 +22,39 @@ func TestSetValue(t *testing.T) {
 
 	handler.setValue("key value")
 
-  assert.Equal(t, handler.store["key"], "value", "they should be equal")
+  assert.Equal(t, handler.store["key"], "value")
+}
+
+func TestGetValue(t *testing.T) {
+	conn := ConnectionMock{}
+	storage := make(map[string]string)
+
+	storage["key"] = "value"
+	handler := sessionHandler{&conn, storage, "", true}
+
+	handler.getValue("key")
+
+  assert.Equal(t, "value\n", conn.String())
+}
+
+
+func TestAuth(t *testing.T) {
+	conn := ConnectionMock{}
+	storage := make(map[string]string)
+
+	handler := sessionHandler{&conn, storage, "password", false}
+
+	// Invalid password
+
+	handler.auth("invalid_password")
+
+  assert.Equal(t, "-ERR invalid password\n", conn.String())
+
+  conn.Reset()
+
+  // Valid password
+
+  handler.auth("password")
+  assert.Equal(t, "+OK\n", conn.String())
+  assert.Equal(t, true, handler.authenticated)
 }
