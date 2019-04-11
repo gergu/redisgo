66f13bc64f4d544a8e31f718ea748dca2a42a2f5
Grzegorz Korzeniowski <korzeniowski.g@gmail.com>
2019-04-11 09:29:44 +0200

    Cover more code with tests

--- session_handler.go
+++ session_handler.go
@@ -23,47 +23,51 @@ func (s *sessionHandler) handleConnection() {
   for {
     msg, err := textproto.NewReader(buf).ReadLine()
 
-    command := strings.SplitN(msg, " ", 2)
-    if len(command) < 2 {
-      s.conn.Write([]byte("Invalid instruction. Use following pattern: GET/SET key [value]\n"))
-      continue
-    }
-
     if err != nil {
       fmt.Printf("Connection closed.\n")
       break
     }
 
-    if command[0] == "AUTH" {
-      s.auth(command[1])
-      continue
-    }
+    s.handleCommand(msg)
+  }
+}
 
-    if !s.authenticated {
-      s.conn.Write([]byte("-NOAUTH Authentication required.\n"))
-      continue
-    }
+func (s *sessionHandler) handleCommand(msg string) {
+  command := strings.SplitN(msg, " ", 2)
+  if len(command) < 2 {
+    s.conn.Write([]byte("Invalid instruction. Use following pattern: GET/SET key [value]\n"))
+    return
+  }
 
-    switch command[0] {
-    case "GET":
-      s.getValue(command[1])
-    case "SET":
-      s.setValue(command[1])
-    default:
-      s.conn.Write([]byte("Unknown command.\n"))
-    }
+  if command[0] == "AUTH" {
+    s.auth(command[1])
+    return
+  }
+
+  if !s.authenticated {
+    s.conn.Write([]byte("-NOAUTH Authentication required.\n"))
+    return
+  }
+
+  switch command[0] {
+  case "GET":
+    s.getValue(command[1])
+  case "SET":
+    s.setValue(command[1])
+  default:
+    s.conn.Write([]byte("Unknown command.\n"))
   }
 }
 
 func (s *sessionHandler) setValue(command string) {
   instruction := strings.SplitN(command, " ", 2)
-  if len(command) < 2 {
+  if len(instruction) < 2 {
     s.conn.Write([]byte("Invalid SET syntax. Use GET key value\n"))
     return
   }
 
   s.store[instruction[0]] = instruction[1]
-  s.conn.Write([]byte("OK\n"))
+  s.conn.Write([]byte("+OK\n"))
 }
 
 func (s *sessionHandler) getValue(key string) {
--- session_handler_test.go
+++ session_handler_test.go
@@ -20,9 +20,14 @@ func TestSetValue(t *testing.T) {
 
 	handler := sessionHandler{&conn, storage, "", true}
 
-	handler.setValue("key value")
+  handler.setValue("key")
+  assert.Equal(t, handler.store["key"], "")
+  assert.Equal(t, "Invalid SET syntax. Use GET key value\n", conn.String())
+  conn.Reset()
 
+	handler.setValue("key value")
   assert.Equal(t, handler.store["key"], "value")
+  assert.Equal(t, "+OK\n", conn.String())
 }
 
 func TestGetValue(t *testing.T) {
@@ -33,7 +38,6 @@ func TestGetValue(t *testing.T) {
 	handler := sessionHandler{&conn, storage, "", true}
 
 	handler.getValue("key")
-
   assert.Equal(t, "value\n", conn.String())
 }
 
@@ -57,4 +61,26 @@ func TestAuth(t *testing.T) {
   handler.auth("password")
   assert.Equal(t, "+OK\n", conn.String())
   assert.Equal(t, true, handler.authenticated)
+}
+
+
+func TestHandleCommand(t *testing.T) {
+	conn := ConnectionMock{}
+	storage := make(map[string]string)
+
+	handler := sessionHandler{&conn, storage, "password", false}
+	handler.handleCommand("GET key")
+  assert.Equal(t, "-NOAUTH Authentication required.\n", conn.String())
+  conn.Reset()
+
+  handler.password = ""
+  handler.authenticated = true
+
+  handler.handleCommand("GET")
+  assert.Equal(t, "Invalid instruction. Use following pattern: GET/SET key [value]\n", conn.String())
+  conn.Reset()
+
+  handler.handleCommand("UNKNOWN parameter param")
+  assert.Equal(t, "Unknown command.\n", conn.String())
+  conn.Reset()
 }
